5c679de2bbbf6efee82773929f535a0aad8a73d0
gowthamRaj-sudo <gowthamraj87544@gmail.com>
2025-01-29 17:16:45 +0530

    testing commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 function App() {
+
+const insta=axios.create({
+  baseURL:"http://192.168.101.76:3001"
+})
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Fetch data using Axios
+    insta
+      .get('/data') // Example API
+      .then((response) => {
+        setData(response.data); // Store response data in state
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+console.log(data)
   return (
     <div className="App">
+      {data.map((e)=>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={e.image} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +39,8 @@ function App() {
           Learn React
         </a>
       </header>
+    
+    )}
     </div>
   );
 }
